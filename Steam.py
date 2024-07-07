18d36f22b2513889f1b3b92d76c7c0c1fcb37599
Dezhriel <143016251+Dezzie07@users.noreply.github.com>
2024-07-07 17:14:54 +0200

    Update Steam.py

--- Steam.py
+++ Steam.py
@@ -1,35 +1,34 @@
 import CoolProp.CoolProp as CP
 
-# Given data
+# Given
 p = 1e5  # pressure [Pa] (1 bar)
 m_dot = 2  # mass flow rate [kg/s]
 L = 2257 * 1e3  # latent heat steam [J/kg]
 heating_demand = 5000  # heating demand [kW]
-temperature_in = 100 + 273.15  # 100°C in Kelvin
-temperature_out = 120 + 273.15  # 120°C in Kelvin
+temperature_in = 100 + 273.15  # Kelvin
+temperature_out = 120 + 273.15  # Kelvin
 
-# Heat recovery rate with latent heat
-Q = m_dot * L / 1e3  # [kW]
+# Heat recovery rate
+Q = m_dot * L / 1e3 
 print(f"Heat recovery rate: {Q} kW")
 
 # Enthalpies from given temperature
-h_in = CP.PropsSI('H', 'P', p, 'T', temperature_in, 'Water')  # J/kg
-h_out = CP.PropsSI('H', 'P', p, 'T', temperature_out, 'Water')  # J/kg
-
+h_in = CP.PropsSI('H', 'P', p, 'T', temperature_in, 'Water')  
+h_out = CP.PropsSI('H', 'P', p, 'T', temperature_out, 'Water')  
 print(f"Enthalpy at inlet: {h_in / 1000} kJ/kg")
 print(f"Enthalpy at outlet: {h_out / 1000} kJ/kg")
 
-# The required heat input using enthalpies and mass flow rate. ( Q = m\delta(H))
-required_heat_input = m_dot * (h_out - h_in) / 1e3  # Also conversion to kW
-print(f"Required heat input: {required_heat_input:.2f} kW")
+# The required heat with enthalpies and mass flow rate. ( Q = m*\delta(H))
+required_heat_input = m_dot * (h_out - h_in) / 1e3 
+print(f"Required heat input: {required_heat_input} kW")
 
 # COP using heat demand
 COP = heating_demand / required_heat_input
-print(f"COP: {COP:.2f}")
+print(f"COP: {COP}")
 
 
 if Q >= required_heat_input:
-    print("The heat recovery system meets the heat demand.")
+  print("The heat recovery system meets the heat demand.")
 else:
-    print("The heat recovery system does not meet the given heat demand. Additional heat source is required.")
+   print("The heat recovery system does not meet the heat demand. Additional heat source is required.")
 
